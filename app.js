68d6401efe55ed099b3ac404a899bd17430cb2ca
Timi Adewumi <oluwatimilehin.adewumi@exquisappfactory.com>
2022-02-20 17:07:44 +0100

    Use the FS Module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-console.log(`Total Memory: ${totalMemory}`);
+fs.readdir('./', function(err, files){
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
