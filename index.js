9c7a5e36b532e97feb03a889a912fd2b75fa28ed
Timi Adewumi <oluwatimilehin.adewumi@exquisappfactory.com>
2022-02-21 10:20:23 +0100

    POST requests

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -16,15 +18,20 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send(`The course with the given ID was not found`);
-    res.send(course);
-});
+app.post('/api/courses', (req, res) => {
 
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+    console.log(course);
+});
 
 
 const port = process.env.PORT || 3000;
 // app.listen(3000, () => console.log(`Listening on port 3000!!...`)); 
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
