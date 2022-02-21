f348aeb1e9ce1cb51ad71dec58067f59be73a083
Timi Adewumi <oluwatimilehin.adewumi@exquisappfactory.com>
2022-02-21 11:39:23 +0100

    DELETE requests

--- index.js
+++ index.js
@@ -20,19 +20,16 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found');
+    if (!course) return res.status(404).send('The course with the given ID was not found');
     res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
     const result = validateCourse(req.body);
     const { error } = validateCourse(req.body); //result.error
-    if (error) {
+    if (error) return res.status(400).send(result.error.details[0].message);
         // 400 Bad Request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
+        
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -45,17 +42,15 @@ app.put('/api/courses/:id', (req, res) => {
     // Look up the course
     // If not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found');
+    if (!course) return res.status(404).send('The course with the given ID was not found');
 
     // Validate
     // If invalid, return 400 - Bad Request
     const result = validateCourse(req.body);
     const { error } = validateCourse(req.body); //result.error
-    if (error) {
+    if (error)
         // 400 Bad Request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+        return res.status(400).send(result.error.details[0].message);
 
     // Update course
     // Return the updated course 
@@ -63,6 +58,19 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course)
 })
 
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // Not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found');
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+    // Return the same course
+});
 
 
 function validateCourse(course) {
