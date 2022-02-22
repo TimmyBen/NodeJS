7fec4783e74e193fe96b5f5bfba4c8d9f0497bbf
Timi Adewumi <oluwatimilehin.adewumi@exquisappfactory.com>
2022-02-22 13:35:01 +0100

    Other inbuilt Middleware

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(logger);
 
 app.use(function(req, res, next) {
     console.log('Authenticating...');
